3b98ed432cfd2f8c11c40c1b9d24c14037cc4da0
willsn <willsn@gmail.com>
2020-06-25 15:05:57 -0600

    first hackerrank in this repo

--- hackerrank.py
+++ hackerrank.py
@@ -1,1 +1,15 @@
-#https://www.hackerrank.com/challenges/list-comprehensions/problem
+#https://www.hackerrank.com/challenges/np-arrays/problem
+
+import numpy
+
+def arrays(arr):
+
+    wills_array = numpy.array(arr, float)
+    wills_array = wills_array[::-1]
+    return wills_array
+    # complete this function
+    # use numpy.array
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
